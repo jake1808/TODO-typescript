4127a223ba878572e5dc35deb24cc2ecb1e378f8
jake1808 <mutale1808@gmail.com>
2023-01-04 16:39:57 +0200

    var, let and const

--- app.js
+++ app.js
@@ -17,10 +17,37 @@
 //2.TEMPLATE STRINGS
 // Template strings allows you to construct a string
 // that includes the values of your variables.
-var container = document.getElementById('container');
-var todo = {
-    id: 123,
-    name: 'pick up drycleaning',
-    completed: true
-};
-container.innerHTML = "\n<div todo=".concat(todo.id, " class=\"list-group-item\">\n    <i class=\"").concat(todo.completed ? "" : "hidden", " text-success glyphicon glyphicon-ok\"></i>\n    <span class=\"name\">").concat(todo.name, "</span>\n</div>\n");
+// let container = document.getElementById('container');
+// let todo = {
+//     id:123,
+//     name:'pick up drycleaning',
+//     completed: true
+// }
+// container.innerHTML = `
+// <div todo=${todo.id} class="list-group-item">
+//     <i class="${todo.completed?"":"hidden"} text-success glyphicon glyphicon-ok"></i>
+//     <span class="name">${todo.name}</span>
+// </div>
+// `
+/*------------------------------------------------------------------------*/
+//3. VAR, LET AND CONST
+// The var keyword allows you to define a variable and that variable is not restricted to a scope
+// var container = document.getElementById('container');
+// for (var x=0; x <= 5; x++){
+//     var counter =x;
+// }
+// console.log(counter);
+// The let keyword allows you to define a variable and that variable is block scoped
+// let container = document.getElementById('container');
+// for (let x = 0; x <= 5; x++) {
+//     let counter = x;    
+// }
+// console.log(counter);
+// The const keyword allows you to define a variable but that variable is immutable meaning it can not be changed and the variable is block scoped.
+// const container = document.getElementById('container');
+// for (let x=0; x<=5; x++) {
+//     const counter=x;
+//     counter=3
+// }
+// console.log(counter);
+/*------------------------------------------------------------------------*/
--- app.ts
+++ app.ts
@@ -27,17 +27,49 @@
 // Template strings allows you to construct a string
 // that includes the values of your variables.
 
-let container = document.getElementById('container');
+// let container = document.getElementById('container');
 
-let todo = {
-    id:123,
-    name:'pick up drycleaning',
-    completed: true
-}
+// let todo = {
+//     id:123,
+//     name:'pick up drycleaning',
+//     completed: true
+// }
 
-container.innerHTML = `
-<div todo=${todo.id} class="list-group-item">
-    <i class="${todo.completed?"":"hidden"} text-success glyphicon glyphicon-ok"></i>
-    <span class="name">${todo.name}</span>
-</div>
-`
+// container.innerHTML = `
+// <div todo=${todo.id} class="list-group-item">
+//     <i class="${todo.completed?"":"hidden"} text-success glyphicon glyphicon-ok"></i>
+//     <span class="name">${todo.name}</span>
+// </div>
+// `
+
+/*------------------------------------------------------------------------*/ 
+
+//3. VAR, LET AND CONST
+// The var keyword allows you to define a variable and that variable is not restricted to a scope
+
+    // var container = document.getElementById('container');
+    // for (var x=0; x <= 5; x++){
+    //     var counter =x;
+    // }
+    // console.log(counter);
+
+// The let keyword allows you to define a variable and that variable is block scoped
+
+    // let container = document.getElementById('container');
+    // for (let x = 0; x <= 5; x++) {
+    //     let counter = x;    
+    // }
+    // console.log(counter);
+
+// The const keyword allows you to define a variable but that variable is immutable meaning it can not be changed and the variable is block scoped.
+
+    // const container = document.getElementById('container');
+
+    // for (let x=0; x<=5; x++) {
+    //     const counter=x;
+    //     counter=3
+    // }
+
+    // console.log(counter);
+
+/*------------------------------------------------------------------------*/ 
