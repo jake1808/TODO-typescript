50e5a6a271a6cd6d5fed9e827d1ed2b687294316
Jacob Mutale <JMutale@sybrin.co.za>
2023-01-04 15:28:19 +0200

    first commit

---
+++ app.ts
@@ -0,0 +1,35 @@
+//ECMASCRIPT FEATURES
+/*------------------------------------------------------------------------*/ 
+
+//1. DEFAULT PARAMETERS
+
+// Allows you to specify a default value if no value is passed
+// into the function
+
+// let container = document.getElementById('container');
+
+// function countDown(initial, final=0, interval=1){
+//     let current = initial;
+
+//     while (current> final) {
+//         container.innerHTML = current;
+//         current -= interval;
+//     }
+// }
+
+// countDown(10);
+
+/*------------------------------------------------------------------------*/ 
+
+//2.TEMPLATE STRINGS
+
+// Template strings allows you to construct a string
+// that includes the values of your variables.
+
+let container = document.getElementById('container');
+
+let todo = {
+    id:123,
+    name:'pick up drycleaning',
+    completed: true
+}
